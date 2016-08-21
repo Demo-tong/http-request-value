777843d0ee76e3640495508878c159d8dc2be0f9
Demo-tong <18829292377@163.com>
2016-08-21 11:26:12 +0800

    tongyang - separate post method

--- spec/demo/http-request-value_spec.js
+++ spec/demo/http-request-value_spec.js
@@ -4,75 +4,78 @@
 'use strict';
 
 let supertest = require('supertest');
-let request = supertest('localhost:3000');
+let app = require('../../demo/http-request-value');
+let request = supertest(app);
 
-describe("http-request-value", function () {
+describe("get", ()=> {
   it("should get parameter by query", (done)=> {
     request
-            .get('/query/?name=1234')
-            .expect({"name": "1234"})
-            .end((err, res)=> {
-              if (err) {
-                done.fail(err);
-              } else {
-                done();
-              }
-            });
+      .get('/query/?name=1234')
+      .expect({"name": "1234"})
+      .end((err, res)=> {
+        if (err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      });
   });
 
   it("should get parameter by params", (done)=> {
     request
-            .get('/params/Demo_tong')
-            .expect({name: "Demo_tong"})
-            .end((err, res)=> {
-              if (err) {
-                done.fail(err);
-              } else {
-                done();
-              }
-            });
+      .get('/params/Demo_tong')
+      .expect({name: "Demo_tong"})
+      .end((err, res)=> {
+        if (err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      });
   });
+});
 
+describe("post", ()=> {
   it("should get parameter by header", (done)=> {
     request
-            .post('/header')
-            .set({value: '1234'})
-            .expect('1234')
-            .end((err, res)=> {
-              if (err) {
-                done.fail(err);
-              } else {
-                done();
-              }
-            });
+      .post('/header')
+      .set({value: '1234'})
+      .expect('1234')
+      .end((err, res)=> {
+        if (err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      });
   });
 
   it("should get parameter by json", (done)=> {
     request
-            .post('/json')
-            .set("Content-Type", "application/json")
-            .send({"name": "zhang", "age": "13"})
-            .expect({"name": "zhang", "age": "13"})
-            .end((err, res)=> {
-              if (err) {
-                done.fail(err);
-              } else {
-                done();
-              }
-            });
+      .post('/json')
+      .set("Content-Type", "application/json")
+      .send({"name": "zhang", "age": "13"})
+      .expect({"name": "zhang", "age": "13"})
+      .end((err, res)=> {
+        if (err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      });
   });
 
   it("should get parameter by urlencoded", (done)=> {
     request
-            .post('/url')
-            .send({name: "zhangsan"})
-            .expect("zhangsan")
-            .end((err, res)=> {
-              if (err) {
-                done.fail(err);
-              } else {
-                done();
-              }
-            });
+      .post('/url')
+      .send({name: "zhangsan"})
+      .expect("zhangsan")
+      .end((err, res)=> {
+        if (err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      });
   });
 });
